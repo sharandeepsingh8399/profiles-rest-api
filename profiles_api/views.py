dc4844dddfda3a900ae0b734676bf214fd63570c
sharan <sharansandhu8399@gmail.com>
2019-09-14 18:43:34 +0530

    loogin api code

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -5,6 +5,8 @@ from rest_framework import viewsets
 from rest_framework import filters
 from profiles_api import serializers
 from profiles_api import models
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from rest_framework.authentication import TokenAuthentication
 from profiles_api import permissions
@@ -106,3 +108,8 @@ class UserProfileViewset(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name','email',)
+
+
+class UserLoginApiView(ObtainAuthToken):
+   """Handle creating user authentication tokens"""
+   renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
